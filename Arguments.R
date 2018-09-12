5ccfdb46b990f68396e513cebc61345da5401832
ngeshlew <ngugilewis@gmail.com>
2018-09-12 07:49:21 +0300

    Arguments

---
+++ Arguments.R
@@ -0,0 +1,15 @@
+Sys.Date()
+#Take no arguments
+
+factorial(4)
+#Takes a single argument
+
+paste("Hello","World", sep = " ")
+#Take mutliple arguments
+
+args(log)
+#Shows which arguments are in a function
+
+?log
+#See what the arguments do
+#Base is an optional argument
